832f348f3c510a79dc60adb39439a0579df83745
Luca Maragnani <luca.maragnani@fastweb.it>
2016-08-04 11:35:50 +0200

    opendcim-4.3

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+VERSION=4.3
+
  cd /var/www
- wget http://opendcim.org/packages/openDCIM-4.2.tar.gz
- tar zxpvf openDCIM-4.2.tar.gz
- ln -s openDCIM-4.2-Release dcim
- rm openDCIM-4.2.tar.gz
+ wget http://opendcim.org/packages/openDCIM-$VERSION.tar.gz
+ tar zxpvf openDCIM-$VERSION.tar.gz
+ ln -s openDCIM-$VERSION dcim
+ rm openDCIM-$VERSION.tar.gz
  rm -R /var/www/html
  chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings /var/www/dcim/images
  
