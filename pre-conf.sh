17640f10059d03a6c05ef4f3e9cff2e3846a1761
Luca Maragnani <luca.maragnani@fastweb.it>
2016-08-24 13:59:23 +0200

    use db from dblink

--- pre-conf.sh
+++ pre-conf.sh
@@ -12,7 +12,10 @@ VERSION=4.3
  
  cd /var/www/dcim
  cp db.inc.php-dist db.inc.php
- 
+
+# use db from docker link 
+sed -i "s/[$]dbhost = [']localhost[']/\$dbhost = 'db'/" /var/www/dcim/db.inc.php
+
  #to fix error relate to ip address of container apache2
  echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
  ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
