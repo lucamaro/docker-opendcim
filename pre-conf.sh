5b586776b56b4e796c37c5c74f41056a43071180
Luca Maragnani <luca.maragnani@fastweb.it>
2017-06-26 17:48:28 +0200

    formatting, wget quiet

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-VERSION=4.3
+VERSION=4.4
 
- cd /var/www
- wget http://opendcim.org/packages/openDCIM-$VERSION.tar.gz
- tar zxpvf openDCIM-$VERSION.tar.gz
- ln -s openDCIM-$VERSION dcim
- rm openDCIM-$VERSION.tar.gz
- rm -R /var/www/html
- chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings /var/www/dcim/images
- 
- cd /var/www/dcim
- cp db.inc.php-dist db.inc.php
+cd /var/www
+wget -q http://opendcim.org/packages/openDCIM-$VERSION.tar.gz
+tar zxpvf openDCIM-$VERSION.tar.gz
+ln -s openDCIM-$VERSION dcim
+rm openDCIM-$VERSION.tar.gz
+rm -R /var/www/html
+chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings /var/www/dcim/images
+
+cd /var/www/dcim
+cp db.inc.php-dist db.inc.php
 
 # use db from docker link 
 sed -i "s/[$]dbhost = [']localhost[']/\$dbhost = 'db'/" /var/www/dcim/db.inc.php
 
- #to fix error relate to ip address of container apache2
- echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
- ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
- 
- #  copy conf of 
- cat << EOF > /var/www/dcim/.htaccess
+#to fix error relate to ip address of container apache2
+echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
+ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
+
+#  copy conf of 
+cat << EOF > /var/www/dcim/.htaccess
 AuthType Basic
 AuthName "openDCIM"
 AuthUserFile /var/www/opendcim.password
 Require valid-user
 EOF
  
- htpasswd -cb /var/www/opendcim.password dcim dcim
- a2enmod rewrite
+htpasswd -cb /var/www/opendcim.password dcim dcim
+a2enmod rewrite
 
 # add locales
 locale-gen az_AZ
@@ -48,4 +48,4 @@ locale-gen zh_CN
 dpkg-reconfigure locales 
 
 # FIX openDCIM code for locale not working
-sed -i 's:putenv("LC_ALL=$locale");:putenv("LANGUAGE=$locale");:' /var/www/dcim/misc.inc.php
+#sed -i 's:putenv("LC_ALL=$locale");:putenv("LANGUAGE=$locale");:' /var/www/dcim/misc.inc.php
